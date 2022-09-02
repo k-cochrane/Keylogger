4d2c2f1b2f28109f33764277f9adf5436abfc7d4
Kevin Cochrane <kevin.cochrane@gmail.com>
2022-09-02 18:03:04 +0200

    Removing info that shouldn't be included.

--- klogger.py
+++ klogger.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
 import keylogger
 
-my_keylogger = keylogger.Keylogger(120, "emailhere", "passwordhere")
+my_keylogger = keylogger.Keylogger(120, "emailhere", "paseswordhere")
 #my_keylogger.start()
